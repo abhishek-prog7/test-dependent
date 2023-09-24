03dcee5045eaf79e5baf648af30f5d1c139c6972
abhishek <abhishekchauhan10x@gmail.com>
2023-09-24 06:21:01 -0400

    initialized

---
+++ src/main/java/com/test/depend/messageSource.java
@@ -0,0 +1,10 @@
+package com.test.depend;
+
+public class messageSource {
+    
+    private String message = "Hello from the source message clas";
+
+    public String getMessage(){
+        return message;
+    }
+}
