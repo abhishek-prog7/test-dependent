e7bdea6e76224817a96108c3c18e147e98be20ff
abhishek-prog7 <72608278+abhishek-prog7@users.noreply.github.com>
2023-09-25 18:41:59 +0530

    Update messageSource.java

--- src/main/java/com/test/depend/messageSource.java
+++ src/main/java/com/test/depend/messageSource.java
@@ -2,7 +2,7 @@ package com.test.depend;
 
 public class messageSource {
     
-    private String message = "Hello from the source message clas";
+    private String message = "Hello from the source message class";
 
     public String getMessage(){
         return message;
